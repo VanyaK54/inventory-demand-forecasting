f6a2e4a8e96aa59cee392e6331121dd3606e74be
vanyaK54 <vanyakaushik54@gmail.com>
2025-06-22 17:31:03 -0400

    add model_prophet

--- src/model_prophet.py
+++ src/model_prophet.py
@@ -1,10 +1,22 @@
 from prophet import Prophet
 import pandas as pd
 
-def prophet_forecast(df):
-    df = df.rename(columns={'date': 'ds', 'units_sold': 'y'})
-    model = Prophet()
-    model.fit(df)
-    future = model.make_future_dataframe(periods=30)
-    forecast = model.predict(future)
-    return forecast
+def prepare_prophet_data(df, date_col='date', target_col='units_sold'):
+    df_prophet = df[[date_col, target_col]].copy()
+    df_prophet.columns = ['ds', 'y']
+    return df_prophet
+
+def forecast_with_prophet(df, holidays_df=None, periods=30):
+    df_prophet = prepare_prophet_data(df)
+
+    if holidays_df is not None:
+        holidays_df.columns = ['ds', 'holiday', 'country']
+        m = Prophet(holidays=holidays_df[['ds', 'holiday']])
+    else:
+        m = Prophet()
+
+    m.fit(df_prophet)
+    future = m.make_future_dataframe(periods=periods)
+    forecast = m.predict(future)
+
+    return forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
